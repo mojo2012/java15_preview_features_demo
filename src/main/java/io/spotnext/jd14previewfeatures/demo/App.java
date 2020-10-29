dfbc66b7e993d2685b4cac82d5f3729038adbac1
Matthias Fuchs <meister.fuchs@gmail.com>
2020-10-29 14:53:07 +0100

    init

---
+++ src/main/java/io/spotnext/jd14previewfeatures/demo/App.java
@@ -0,0 +1,11 @@
+package io.spotnext.jd14previewfeatures.demo;
+
+public class App {
+    public static void main(String[] args) {
+        final var text = """
+                test
+                """;
+
+        System.out.println(text);
+    }
+}
